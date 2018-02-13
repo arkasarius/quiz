ada2df9ddcd99397e8ccc031a3ed09ca555d03ca
arkasarius <roger.ordonez.brumos@gmail.com>
2018-02-13 11:31:18 +0100

    vincular strings amb recurs de text

--- app/src/main/java/com/example/roger/quiz/quizactivity.java
+++ app/src/main/java/com/example/roger/quiz/quizactivity.java
@@ -2,12 +2,29 @@ package com.example.roger.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class quizactivity extends AppCompatActivity {
+
+    private final int[] ids_buttons={
+            R.id.answer1,
+            R.id.answer2,
+            R.id.answer3,
+            R.id.answer4
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizactivity);
+        TextView question_text = findViewById(R.id.question_text);
+        question_text.setText(R.string.question_content);
+        String[] answer_text = getResources().getStringArray(R.array.answer_text);
+        for(int i =0;i<answer_text.length;i++){
+            RadioButton rb = findViewById(ids_buttons[i]);
+            rb.setText(answer_text[i]);
+        }
+
     }
 }
